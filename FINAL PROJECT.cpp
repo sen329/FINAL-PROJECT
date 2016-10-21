12d36161cfd361ea698e80f6a9370279953edd03
sen329 <hans.hilman@gmail.com>
2016-10-21 17:08:23 +0700

    UPDATE

--- FINAL PROJECT.cpp
+++ FINAL PROJECT.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 void StartStory();
+void StorySection1();
+void StoryDeath1();
 
 int main(){
 	int option;
@@ -27,7 +29,7 @@ int main(){
 			cout << "Exiting"<<endl;
 		}
 		else {
-			cout << "Please choose a valid option";
+			cout << "Please choose a valid option" <<endl;
 		
 		}
 	}
@@ -38,18 +40,51 @@ int main(){
 
 void StartStory()
 {
-	string choice;
+	int choice;
 	
-	cout << "This is where story begins"<<endl;
+	cout << "You wake up in a dimly-lit room."<<endl;
 	system ("pause");
-	cout << "die or live? ";
+	cout << "You looked around, wondering how you got here."<<endl;
+	system ("pause");
+	cout << "You remembered that you came home from work"<<endl;
+	cout << "and fallen asleep on your bed."<<endl;
+	system("pause");
+	cout << "You see a flashlight that is turned on."<<endl;
+	cout << "Take the flashlight? "<<endl;
+	cout << "1. Take"<<endl;
+	cout << "2. Don't take"<<endl;
 	cin >> choice;
 	
-	if(choice == "die"){
-		cout << "you died"<<endl;
-		
+	if(choice == 1){
+		cout << "You have taken the flashlight."<<endl;
+		system("pause");
+		StorySection1();
 	}
-	else if (choice == "live"){
-		cout << "for now this is the end"<<endl;	
+	else if (choice == 2){
+		cout << "You did not take the flashlight."<<endl;
+		system("pause");
+		StoryDeath1();	
 	}
+}
+
+void StorySection1()
+{
+	cout << "For now this is the end"<<endl;
+	system("pause");
+}
+
+void StoryDeath1()
+{
+	cout << "You walked outside the room with no light."<<endl;
+	cout << "It is pitch black because the lights are off"<<endl;
+	cout << "and you have no source of light to help."<<endl;
+	system("pause");
+	cout << "Suddenly, you hear a metalic noise coming towards you."<<endl;
+	system("pause");
+	cout << "Then you feel a sharp pain on your stomach"<<endl;
+	system("pause");
+	cout << "You were stabbed by and unknown attacker"<<endl;
+	system("pause");
+	cout << "You died"<<endl;
+	system("pause");
 }
